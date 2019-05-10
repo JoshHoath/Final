77d27b5a9ae24f9a772becccd5918cdac1b25f19
JoshHoath <mrjoshuahoath@gmail.com>
2019-05-10 10:44:52 -0600

    inital commit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,7 +8,7 @@
     </head>
     <body>
         <div id="app">
-            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-custom">
                 <a class="navbar-brand" href="/tweets">Tweeter</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
